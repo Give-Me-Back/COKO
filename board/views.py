9d606869abd46b61eba6e624d50a947c70de4357
xc7230 <xc7230@naver.com>
2022-12-09 23:28:20 +0900

    ajax test

--- board/views.py
+++ board/views.py
@@ -1,18 +1,25 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from django.db.models import Q
 from board.models import Corona
 from datetime import datetime, timedelta
+from board.models import News
 
 def list(request):
-    posts = Corona.objects.all()
+    posts = Corona.objects.all().order_by('-localocccnt')
+    g_posts = Corona.objects.all().order_by('stdday')
+    g_posts = g_posts.values()
+    g_posts =g_posts.filter(gubun__icontains="합계")
+    print("ggggggg", g_posts)
     q = datetime.now().date() - timedelta(1)
-    print(q)
+    print(posts)
     if q:
         posts = posts.filter(stdday__icontains=f"{q}")
         print(type(posts))
-        print(posts)
-        return render(request, 'board/index.html', {'posts': posts, 'q': q})
+        return render(request, 'board/index.html', {'posts': posts, 'q': q, 'g_posts':g_posts })
 
     else:
         return render(request, 'board/index.html')
@@ -21,6 +28,7 @@ def read(request):
     posts = Corona.objects.values()
     q = datetime.now().date() - timedelta(1)
     print(q)
+
     if q:
         posts = posts.filter(stdday__icontains=f"{q}")
         print(type(posts))
@@ -29,4 +37,19 @@ def read(request):
 
     else:
         return render(request, 'board/list.html')
+
+# def News(request):
+#     posts = News.objects.all()
+#     return render(request, 'board/news.html', {'posts': posts})
+
+
+def News_list(request):
+    posts = News.objects.all()
+    return render(request, 'board/news.html', {'posts': posts})
+
+def regions(request):
+    gubun=request.GET.get('gubun')
+    print(gubun)
+    context = {'gubun': gubun}
+    return JsonResponse(context)
 
